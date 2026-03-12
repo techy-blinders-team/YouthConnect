4f1c6fd8d222ddf75a0a798ab034afa06b195408
Edriane <doidoiwey@gmail.com>
2026-03-12 20:00:48 +0800

    feat: add web configuration for localhost 4200

--- backend/youthconnect_id/src/main/java/com/youthconnect/youthconnect_id/config/WebConfig.java
+++ backend/youthconnect_id/src/main/java/com/youthconnect/youthconnect_id/config/WebConfig.java
@@ -1,5 +1,31 @@
 package com.youthconnect.youthconnect_id.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+@Configuration
+public class WebConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+
+        config.addAllowedHeader("*");
+    
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedHeader("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        source.registerCorsConfiguration("/api/**", config);
+
+        return (corsFilter());
+    }
 }
