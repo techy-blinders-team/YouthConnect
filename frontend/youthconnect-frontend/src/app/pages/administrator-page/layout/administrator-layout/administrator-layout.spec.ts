de9f6c8808efa1a3835a58f248989e6ece048bb3
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-03-28 06:51:56 +0800

    feat(administrator-layout): add initial layout component

---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/layout/administrator-layout/administrator-layout.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdministratorLayout } from './administrator-layout';
+
+describe('AdministratorLayout', () => {
+  let component: AdministratorLayout;
+  let fixture: ComponentFixture<AdministratorLayout>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdministratorLayout]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdministratorLayout);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/layout/administrator-layout/administrator-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrator-layout',
+  imports: [],
+  templateUrl: './administrator-layout.html',
+  styleUrl: './administrator-layout.scss',
+})
+export class AdministratorLayout {
+
+}
