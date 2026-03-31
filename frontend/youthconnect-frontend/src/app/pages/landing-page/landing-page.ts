9edf22793f733f96721e0d52424907dd08b5cc5e
Edriane <doidoiwey@gmail.com>
2026-03-31 21:52:24 +0800

    feat: add navigation bar into shared component

--- frontend/youthconnect-frontend/src/app/pages/landing-page/landing-page.ts
+++ frontend/youthconnect-frontend/src/app/pages/landing-page/landing-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavigationBar } from "../../shared/navigation-bar/navigation-bar";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [NavigationBar],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
