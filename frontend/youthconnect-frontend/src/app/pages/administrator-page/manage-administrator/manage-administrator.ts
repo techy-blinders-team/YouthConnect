429f5314f6780f0c0a1b77933db9a83147758ca7
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-03 18:15:58 +0800

    feat(manage-administrator): add manage administrator component

---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/manage-administrator/manage-administrator.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-administrator',
+  imports: [],
+  templateUrl: './manage-administrator.html',
+  styleUrl: './manage-administrator.scss',
+})
+export class ManageAdministrator {
+
+}
