5de0e9381816d01f1e13aacc2115f58ff2edbf84
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-05 02:16:36 +0800

    feat(administrator): add initial style to the system statistics

--- frontend/youthconnect-frontend/src/app/pages/administrator-page/system-statistics/system-statistics.ts
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/system-statistics/system-statistics.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-system-statistics',
+  standalone: true,
   imports: [],
   templateUrl: './system-statistics.html',
   styleUrl: './system-statistics.scss',
