ac391b6d8f78421b27d9fe180a07d31d54d47e4e
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-06 00:07:20 +0800

    feat(administrator): add edit administrator component

---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/feature/edit-administrator-feature/edit-administrator-feature.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-administrator-feature',
+  imports: [],
+  templateUrl: './edit-administrator-feature.html',
+  styleUrl: './edit-administrator-feature.scss',
+})
+export class EditAdministratorFeature {
+
+}
