3c7e4c39d9aab7ed51a479cd7deacf34e5342f3d
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-06 20:39:52 +0800

    feat(add-sk): create add-sk component

---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/feature/add-sk/add-sk.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sk',
+  imports: [],
+  templateUrl: './add-sk.html',
+  styleUrl: './add-sk.scss',
+})
+export class AddSk {
+
+}
