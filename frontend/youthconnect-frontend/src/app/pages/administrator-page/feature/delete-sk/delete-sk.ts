7c71df5bebc3e3054b1032352ad132c039e3b4fa
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-06 20:40:18 +0800

    feat(delete-sk): add delete-sk component

---
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/feature/delete-sk/delete-sk.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-sk',
+  imports: [],
+  templateUrl: './delete-sk.html',
+  styleUrl: './delete-sk.scss',
+})
+export class DeleteSk {
+
+}
