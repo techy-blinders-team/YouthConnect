9a4b990ffa147515ff8f6574371ec6acbcb26f5e
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2026-04-10 20:51:03 +0800

    feat(administrator-sidebar): enhance sidebar navigation with active route highlighting

--- frontend/youthconnect-frontend/src/app/pages/administrator-page/layout/administrator-sidebar/administrator-sidebar.ts
+++ frontend/youthconnect-frontend/src/app/pages/administrator-page/layout/administrator-sidebar/administrator-sidebar.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { AuthService } from '../../../../services/auth.service';
 import { AdminInfoService } from '../../../../services/admin-auth.service';
@@ -11,10 +11,15 @@ import { AdminInfoService } from '../../../../services/admin-auth.service';
   styleUrl: './administrator-sidebar.scss',
 })
 export class AdministratorSidebar {
+  router = inject(Router);
   authService = inject(AuthService);
   adminInfoService = inject(AdminInfoService);
   
   adminInfo$ = this.adminInfoService.adminInfo$;
+
+  isRouteActive(path: string): boolean {
+    return this.router.url === path;
+  }
 
   logout(): void {
     this.adminInfoService.clearAdminInfo();
