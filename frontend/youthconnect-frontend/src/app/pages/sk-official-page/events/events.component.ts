37b6068c0e536f0248d3ed3c503648caccd42379
consultado-kirby <kirbyconsultado@gmail.com>
2026-04-11 22:40:44 +0800

    feat: (sk-official): add an event creation modal

--- frontend/youthconnect-frontend/src/app/pages/sk-official-page/events/events.component.ts
+++ frontend/youthconnect-frontend/src/app/pages/sk-official-page/events/events.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class EventsComponent {}
+export class EventsComponent {
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  createEvent() {
+    // TODO: Add event creation logic
+    this.closeModal();
+  }
+}
