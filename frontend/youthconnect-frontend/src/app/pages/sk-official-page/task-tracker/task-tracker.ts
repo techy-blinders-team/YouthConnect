cf36d0a6560698de8d80bfac2e17ae3b8457f816
consultado-kirby <kirbyconsultado@gmail.com>
2026-04-12 18:56:34 +0800

    feat: add the task tracker page and it's header with user's profile

--- frontend/youthconnect-frontend/src/app/pages/sk-official-page/task-tracker/task-tracker.ts
+++ frontend/youthconnect-frontend/src/app/pages/sk-official-page/task-tracker/task-tracker.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-tracker',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-tracker.html',
   styleUrl: './task-tracker.scss',
 })
 export class TaskTracker {
+  isModalOpen = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  createTask() {
+    // TODO: Add task creation logic
+    this.closeModal();
+  }
 }
