affdbd9bf358ed46789aa9b5b65024353d0f2f8d
veslino-marc <veslinomarc4@gmail.com>
2026-04-14 05:00:21 +0800

    feat: enhance the ui ux of the youth sidebar  i add animations, collapsible button and redesign it.

--- frontend/youthconnect-frontend/src/app/pages/youth-user-page/layout/youth-sidebar/youth-sidebar.ts
+++ frontend/youthconnect-frontend/src/app/pages/youth-user-page/layout/youth-sidebar/youth-sidebar.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-youth-sidebar',
-    imports: [RouterLink],
+    imports: [RouterLink, CommonModule],
     templateUrl: './youth-sidebar.html',
     styleUrl: './youth-sidebar.scss',
 })
@@ -12,6 +13,8 @@ export class YouthSidebar implements OnInit {
     router = inject(Router);
     authService = inject(AuthService);
     userEmail: string = '';
+    @Output() collapsedChange = new EventEmitter<boolean>();
+    isCollapsed = false;
 
     ngOnInit(): void {
         const user = this.authService.getCurrentUser();
@@ -20,6 +23,11 @@ export class YouthSidebar implements OnInit {
 
     isRouteActive(path: string): boolean {
         return this.router.url === path;
+    }
+
+    toggleSidebar(): void {
+        this.isCollapsed = !this.isCollapsed;
+        this.collapsedChange.emit(this.isCollapsed);
     }
 
     logout(): void {
