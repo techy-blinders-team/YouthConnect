3676124fa5105e8ad5d4519cf811118265367b2a
consultado-kirby <kirbyconsultado@gmail.com>
2026-04-20 15:26:49 +0800

    feat: add a logging in and logging out loading modal

--- frontend/youthconnect-frontend/src/app/pages/sk-official-page/side-nav-bar/side-nav-bar.ts
+++ frontend/youthconnect-frontend/src/app/pages/sk-official-page/side-nav-bar/side-nav-bar.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-nav-bar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './side-nav-bar.html',
   styleUrl: './side-nav-bar.scss'
 })
 export class SideNavBar {
   private authService = inject(AuthService);
+  isLoggingOut = false;
 
   logout(): void {
-    this.authService.logout();
+    this.isLoggingOut = true;
+    
+    setTimeout(() => {
+      this.authService.logout();
+    }, 1000);
   }
 }
