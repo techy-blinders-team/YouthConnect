befd57b3052993e118f5d0f574be87ddfb00b45e
consultado-kirby <kirbyconsultado@gmail.com>
2026-05-07 18:29:54 +0800

    revert: restores style chages before dashboard and side bar mobile responsive

--- frontend/youthconnect-frontend/src/app/pages/sk-official-page/sk-official-layout/sk-official-layout.ts
+++ frontend/youthconnect-frontend/src/app/pages/sk-official-page/sk-official-layout/sk-official-layout.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
 import { SideNavBar } from '../side-nav-bar/side-nav-bar';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sk-official-layout',
   standalone: true,
-  imports: [SideNavBar, RouterOutlet, CommonModule],
+  imports: [SideNavBar, RouterOutlet],
   templateUrl: './sk-official-layout.html',
   styleUrls: ['./sk-official-layout.scss']
 })
-export class SkOfficialLayout {
-  isSidebarOpen = false;
-
-  toggleSidebar(): void {
-    this.isSidebarOpen = !this.isSidebarOpen;
-  }
-
-  closeSidebar(): void {
-    this.isSidebarOpen = false;
-  }
-}
+export class SkOfficialLayout {}
